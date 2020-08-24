c0bbe4f17e61cbacdc33810c346c3f8d73eeb9d4
eafanase <pankowa@yandex.ru>
2020-08-24 22:10:48 +0300

    Create new class Point, create new class main

--- sandbox/src/main/java/ru/stqa/katja/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/katja/sandbox/MyFirstProgram.java
@@ -3,30 +3,8 @@ package ru.stqa.katja.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Katja");
-
-    double l=5;
-      System.out.println("Площадь квадрата со стороной " +l + "="+ area(l));
-
-      double a = 4;
-      double b = 6;
-      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    Point p1 = new Point(2,3);
+    Point p2 = new Point(5,8);
+    System.out.println("Расстояние между двумя точками " + "= " + p1.distance(p1, p2));
   }
-
-  public static void hello(String somebody) {
-
-    System.out.println("Hello, " + somebody+ "!");
-  }
-
-  public static double area(double len) {
-      return len*len;
-  }
-
-  public static double area (double a, double b) {
-      return a*b;
-  }
-
-
 }
