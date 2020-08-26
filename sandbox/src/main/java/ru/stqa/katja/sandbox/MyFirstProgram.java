c976222ad9194026c32b8905f47ebfdb8739364a
eafanase <pankowa@yandex.ru>
2020-08-26 17:39:37 +0300

    Update in the files according to comment

--- sandbox/src/main/java/ru/stqa/katja/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/katja/sandbox/MyFirstProgram.java
@@ -5,6 +5,6 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     Point p1 = new Point(2,3);
     Point p2 = new Point(5,8);
-    System.out.println("Расстояние между двумя точками " + "= " + p1.distance(p1, p2));
+    System.out.println("Расстояние между двумя точками " + "= " + p1.distance(p2));
   }
 }
