57c53f84bf1f0aba0227aa3209f93fb405aa9996
eafanase <pankowa@yandex.ru>
2020-09-08 10:49:04 +0300

    Update HelperBase for default setting of values (null, existing text)

--- addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/tests/GroupCreationTests.java
@@ -10,7 +10,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "TEST2", "TEST3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returntoGroupPage();
     app.getNavigationHelper().gotoGroupPage();
