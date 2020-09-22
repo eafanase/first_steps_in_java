799a03ce12a05468a1ecf9088de666aa35a3ce1b
eafanase <pankowa@yandex.ru>
2020-09-22 11:13:16 +0300

    Update test: using ids for GroupModificationTest

--- addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/appmanager/GroupHelper.java
@@ -63,8 +63,8 @@ public class GroupHelper extends HelperBase {
     returntoGroupPage();
   }
 
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
