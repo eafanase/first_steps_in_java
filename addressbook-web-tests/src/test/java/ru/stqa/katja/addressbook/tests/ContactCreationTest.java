3ff1c80bc4186f8c91e7fc4e5230dd71beda3753
eafanase <pankowa@yandex.ru>
2020-09-28 00:49:04 +0300

    Update data providers in ContactCreationTest and GroupCreationTest

--- addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/tests/ContactCreationTest.java
@@ -1,48 +1,67 @@
 package ru.stqa.katja.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.katja.addressbook.model.Contact;
 import ru.stqa.katja.addressbook.model.ContactData;
-import ru.stqa.katja.addressbook.model.GroupData;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+
 public class ContactCreationTest extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() throws IOException {
- //   List<Object[]> list= new ArrayList<Object[]>();
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    //   List<Object[]> list= new ArrayList<Object[]>();
 //    File photo = new File("src/test/resources/avatar.png");
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
     String xml = "";
     String line = reader.readLine();
-    while (line != null){
+    while (line != null) {
       xml += line;
-    //  String[] split= line.split(";");
-    //  list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withPhone(split[2]).withGroup(split[3])});
+      //  String[] split= line.split(";");
+      //  list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withPhone(split[2]).withGroup(split[3])});
       line = reader.readLine();
     }
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
- //   return list.iterator();
+    return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+    //   return list.iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    //   List<Object[]> list= new ArrayList<Object[]>();
+//    File photo = new File("src/test/resources/avatar.png");
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+    }.getType());
+    return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+
+    //   return list.iterator();
   }
 
 
-
-  @Test(dataProvider = "validContacts")
+  @Test(dataProvider = "validContactsFromJson")
   public void testContactCreation(ContactData contact) {
     Contact before = app.contact().all();
 //    File photo = new File("src/test/resources/avatar.png");
@@ -56,7 +75,7 @@ public class ContactCreationTest extends TestBase {
     //   app.sessionHelper.logout();
   }
 
-  @Test (enabled = false)
+  @Test(enabled = false)
   public void testCurrentDir() {
     File currentDir = new File(".");
     System.out.println(currentDir.getAbsolutePath());
--- addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/tests/GroupCreationTests.java
@@ -1,12 +1,17 @@
 package ru.stqa.katja.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.katja.addressbook.model.GroupData;
 import ru.stqa.katja.addressbook.model.Groups;
-import java.io.*;
-import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
  //   List<Object[]> list= new ArrayList<Object[]>();
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
     String xml = "";
@@ -31,7 +36,23 @@ public class GroupCreationTests extends TestBase {
     return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
    }
 
-  @Test (dataProvider = "validGroups")
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    //   List<Object[]> list= new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null){
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson =new Gson();
+    List <GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+
+  @Test (dataProvider = "validGroupsFromJson")
  // public void testGroupCreation(String name, String header, String footer) {
   public void testGroupCreation(GroupData group) {
 //    GroupData group = new GroupData().withName("test2");
