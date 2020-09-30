592193fb1b0e27282c80994d108df71260e20b9f
eafanase <pankowa@yandex.ru>
2020-09-30 07:09:02 +0300

    Update generators and data providers:add auto closing of files

--- addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/generators/GroupDataGenerator.java
@@ -52,9 +52,10 @@ public class GroupDataGenerator {
   private void saveAsJson(List<GroupData> groups, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+   try (Writer writer = new FileWriter(file)) {
+     writer.write(json);
+   }
+
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
@@ -62,19 +63,20 @@ public class GroupDataGenerator {
  //   xstream.alias("group", GroupData.class );
     xstream.processAnnotations(GroupData.class );
     String xml =xstream.toXML(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
+
   }
 
   private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
-    Writer writer = new FileWriter(file);
+  try  (Writer writer = new FileWriter(file)){
     for (GroupData group: groups) {
       writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
     }
-    writer.close();
-  }
+    }
+     }
 
   private  List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
