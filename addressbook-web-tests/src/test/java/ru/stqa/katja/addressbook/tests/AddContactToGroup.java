196fd639e310386e1fd279b9b1143b108c42733a
eafanase <pankowa@yandex.ru>
2020-10-07 09:41:38 +0300

    Update test AddContactToGroup.java, add random contact to random group

--- addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/tests/AddContactToGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/katja/addressbook/tests/AddContactToGroup.java
@@ -15,16 +15,17 @@ public class AddContactToGroup extends TestBase {
       app.goTo().groupPage();
       app.group().create(new GroupData().withName("test1"));
     }
-
-    if (app.db().groups().size() == 0){
-      app.goTo().groupPage();
-      app.group().create(new GroupData().withName("test1"));
+    app.goTo().homePage();
+    Groups groups =app.db().groups();
+    if (app.db().contacts().size() == 0) {
+      app.contact().create(new ContactData().withFirstname("Donald").withLastname("Duck"));
+      app.goTo().homePage();
     }
   }
   @Test
   public void testAddContactToGroup() {
     ContactData addedContact = app.db().contacts().iterator().next();
-    Groups GroupsByContact = app.db().groups();
+  //  Groups GroupsByContact = app.db().groups();
 
     GroupData addedGroup = app.db().groups().iterator().next();
      app.goTo().homePage();
