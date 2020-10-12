876c05dfa277a58608c9d0e0f6006b9386e3dda4
eafanase <pankowa@yandex.ru>
2020-10-12 22:43:30 +0300

    Add mantis modul

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.hamcrest.MatcherAssert;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+import java.io.IOException;
+
+
+public class TestBase {
+
+
+  protected static ApplicationManager app;
+
+  static {
+    try {
+      app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @BeforeSuite
+  public void setUp() throws IOException {
+    app.init();
+  }
+
+  @AfterSuite (alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+
+
+
+}
