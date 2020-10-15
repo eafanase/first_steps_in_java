c31ba8f656dfa5f84e336ba0505c455cc8e9d9aa
eafanase <pankowa@yandex.ru>
2020-10-15 13:55:19 +0300

    Передача файлов на удалённую машину по протоколу FTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.File;
 import java.io.IOException;
 
 
@@ -24,10 +26,12 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws IOException {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php_back");
   }
 
   @AfterSuite (alwaysRun = true)
-  public void tearDown() {
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.php_back", "config_inc.php");
     app.stop();
   }
 
